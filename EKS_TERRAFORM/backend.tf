f5ef94ae206ba78d7e6a076fb3f50973418369bb
Charytarak <146544663+charytarak@users.noreply.github.com>
2024-01-16 18:04:41 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mybucket" # Replace with your actual S3 bucket name
+    bucket = "charybucket11" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
